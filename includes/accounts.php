422de66674515b0c9c49cca75303ffa88ad49a58
saib334 <box.saib@gmail.com>
2026-03-11 22:55:38 +0000

    Update accounts.php

--- includes/accounts.php
+++ includes/accounts.php
@@ -230,7 +230,7 @@ public static function trouver_par_login_simple($username) {
 	public static function not_sup_admin(){
 	$q =  "SELECT * FROM ".self::$nom_table;
 	$q .= " WHERE type !='super_admin'";
-	$q .= " AND type !='administrateur'";
+	$q .= " and type !='administrateur'";
     return  self::trouve_par_sql($q);
 	}
 	public static function ens(){
